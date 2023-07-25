49273e6d41a1d4cc6e92ef790c8b23b09c21c329
Ki Jong Byun <kjbyun0@gmail.com>
2023-07-25 04:56:39 -0500

    Assignment Done

--- main.js
+++ main.js
@@ -3,8 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+const hearts = document.getElementsByClassName('like-glyph');
+const arrHearts = [...hearts];
+arrHearts.forEach((elem) => {
+  elem.addEventListener('click', (e) => {
+    if (e.target.textContent === EMPTY_HEART) {
+      mimicServerCall()
+      .then(resp => {
+        e.target.textContent = FULL_HEART;
+        e.target.classList.add('activated-heart');
+      })
+      .catch(error => {
+        const divModal = document.querySelector('#modal');
+        divModal.classList.remove('hidden');
+        const pError = modal.querySelector('#modal-message');
+        pError.textContent = error;
+        setTimeout(() => {
+          divModal.classList.add('hidden');
+        }, 3000);
+      });
+    } else {
+      e.target.textContent = EMPTY_HEART;
+      e.target.classList.remove('activated-heart');
+    }
+  })
+});
 
 
 //------------------------------------------------------------------------------
